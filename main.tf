737cba0823b174849be2c3c2ebf5811922cb4a26
darshanbargal <darshan.bargal@gmail.com>
2026-07-22 14:34:57 +0530

    Lesson: full resource lifecycle with databricks_directory

--- main.tf
+++ main.tf
@@ -1,16 +1,27 @@
 terraform {
-  required_providers {
-    databricks = {
-      source  = "databricks/databricks"
-      version = "~> 1.0"
+    required_providers {
+      databricks = {
+        source = "databricks/databricks"
+      }
     }
-  }
 }
 
-provider "databricks" {}
 
-data "databricks_current_user" "me" {}
+provider "databricks" {
+  
+}
+
+
+
+data "databricks_current_user" "me"{
+
+}
+
 
 output "my_username" {
   value = data.databricks_current_user.me.user_name
+}
+
+resource "databricks_directory" "test_folder" {
+    path = "/Shared/terraform-test-folder-renamed"
 }
