df8b9e676428eee4b91b963ca114cd89b0da232c
darshanbargal <darshan.bargal@gmail.com>
2026-07-22 15:35:11 +0530

    Lesson: databricks_job with serverless task, chained to notebook resource

--- main.tf
+++ main.tf
@@ -26,10 +26,20 @@ resource "databricks_directory" "test_folder" {
     path = "/Shared/terraform-test-folder-renamed"
 }
 
-
-
 resource "databricks_notebook" "my_notebook" {
   path            = "/Shared/terraform-notebook-lesson"
   language        = "PYTHON"
   content_base64  = base64encode("print('hello from terraform')")
+}
+
+resource "databricks_job" "my_job" {
+  name = "terraform-lesson-job"
+
+  task {
+    task_key = "run_notebook"
+
+    notebook_task {
+      notebook_path = databricks_notebook.my_notebook.path
+    }
+  }
 }
